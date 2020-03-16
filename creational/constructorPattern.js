6111ee741d3be859922a615f07e82aa5d0174c03
Levente Fodor <levilevi183@gmail.com>
2020-03-16 16:27:31 +0100

    Update constructorPattern.js with arrow functions refactoring

--- creational/constructorPattern.js
+++ creational/constructorPattern.js
@@ -1,21 +1,21 @@
-var Task = function(name) {
+let Task = function(name) {
     this.name = name;
     this.completed = false;
 
-    this.complete = function() {
+    this.complete = () => {
         console.log(`completing task: ${this.name}`);
         this.completed = true;
     }
 
-    this.save = function() {
+    this.save = () => {
         console.log(`saving task: ${this.name}`);
     }
 }
 
-var task1 = new Task('create a demo for constructors');
-var task2 = new Task('create a demo for modules');
-var task3 = new Task('create a demo for singletons');
-var task4 = new Task('create a demo for prototypes');
+let task1 = new Task('create a demo for constructors');
+let task2 = new Task('create a demo for modules');
+let task3 = new Task('create a demo for singletons');
+let task4 = new Task('create a demo for prototypes');
 
 task1.complete();
 task2.save();
